4d088faee724df77e5984f4630364e96f1e46adf
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-05-20 23:31:19 -0300

    Solution without interfaces

---
+++ course/Services/BrasilTaxService.cs
@@ -0,0 +1,17 @@
+namespace course.Services
+{
+  public class BrasilTaxService
+  {
+    public double Tax(double amount)
+    {
+      if (amount <= 100.0)
+      {
+        return amount * 0.2;
+      }
+      else
+      {
+        return amount * 0.15;
+      }
+    }
+  }
+}
---
+++ course/Services/RentalService.cs
@@ -0,0 +1,39 @@
+using System;
+
+using course.Entities;
+
+namespace course.Services
+{
+  public class RentalService
+  {
+    public double PricePerHour { get; private set; }
+    public double PricePerDay { get; private set; }
+
+    public RentalService(double pricePerHour, double pricePerDay)
+    {
+      PricePerHour = pricePerHour;
+      PricePerDay = pricePerDay;
+    }
+
+		private BrasilTaxService _brasilTaxService = new BrasilTaxService();
+
+    public void ProcessInvoice(CarRental carRental)
+    {
+			TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+			double basicPayment = 0.0;
+			if (duration.TotalHours <= 12.0)
+			{
+				basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+			}
+			else
+			{
+				basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+			}
+
+			double tax = _brasilTaxService.Tax(basicPayment);
+
+			carRental.Invoice = new Invoice(basicPayment, tax);
+    }
+  }
+}
